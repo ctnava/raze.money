8102d0c75ba5d352cd6d97d2efb917e48c5ad406
CyAaron Tai Nava <cyaaron.tai.nava@gmail.com>
2022-07-23 11:31:13 -0500

    two routes that load different contracts

--- src/api/lib/raze/routes.js
+++ src/api/lib/raze/routes.js
@@ -1,36 +1,20 @@
 require("dotenv").config();
 const fs = require("fs");
-// const { uploadPaths, uploadedPaths } = require("./dirs.js");
-// const { uploadLabels, uploadedLabels } = require("./labels.js");
 const {
-  verifyMessage,
-  verifyMessages,
-  getCachedContract,
-} = require("./blockchain.js");
-
-// const { deleteFiles } = require("./cleanup.js");
-// const { garble, quickDecrypt } = require("../utils/encryption");
-// const { performSweep } = require("./cleanup.js");
-// const messageKey = process.env.BC_KEY;
-
-// const {
-//   decomposeFile,
-//   uploadEncrypted,
-//   saveAndValidate,
-//   updatePin,
-//   unpin,
-//   extractKey,
-//   extractKeys,
-//   getFile
-// } = require("./transformation.js");
-// const { findPin } = require("./pins.js");
+  Contract,
+  loadContract,
+  verifySignature,
+  signerAddress,
+} = require("../utils/blockchain.js");
 
 function routeServices(app) {
   app.get("/records", (req, res) => {
+    const RazeMoney = loadContract(req.body.chainId, "RazeMoney");
     res.json("ok");
   });
 
   app.get("/router", (req, res) => {
+    const RazeRouter = loadContract(req.body.chainId, "RazeRouter");
     res.json("ok");
   });
 }
